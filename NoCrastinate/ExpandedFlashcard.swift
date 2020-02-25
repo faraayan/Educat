0cb924f5adda54c6c289af0aa206d352939a0fba
Fara <farayan365@gmail.com>
2020-02-25 00:57:06 -0800

    Add flip card ability

--- NoCrastinate/ExpandedFlashcard.swift
+++ NoCrastinate/ExpandedFlashcard.swift
@@ -10,18 +10,17 @@ import UIKit
 var myIndex = 0
 class ExpandedFlashcard: UIViewController {
     
-    @IBAction func Cancel(_ sender: Any) {
-    }
     @IBOutlet weak var term: UILabel!
     
     @IBOutlet weak var def: UILabel!
     
     @IBOutlet weak var termLabel: UILabel!
     
-    
     @IBOutlet weak var defLabel: UILabel!
     
     @IBOutlet weak var rememberLabel: UILabel!
+    
+    var isHidden = false
     
     @IBAction func didNotRemember(_ sender: Any) {
         remembered[whichFolder][whichFlashcard] = -1
@@ -29,8 +28,7 @@ class ExpandedFlashcard: UIViewController {
         print(remembered)
         dismiss(animated: true, completion: nil)
     }
-    
-    
+
     @IBAction func didRemember(_ sender: Any) {
         remembered[whichFolder][whichFlashcard] = 1
         UserDefaults.standard.set(remembered, forKey: "remembered")
@@ -40,9 +38,54 @@ class ExpandedFlashcard: UIViewController {
     }
    override func viewDidLoad() {
         super.viewDidLoad()
-    print(myIndex)
-    termLabel.text = flashcardsTerm[whichFolder][whichFlashcard]
-    defLabel.text = flashcardsDef[whichFolder][whichFlashcard]
-    }
+        termLabel.text = flashcardsTerm[whichFolder][whichFlashcard]
+        defLabel.text = flashcardsDef[whichFolder][whichFlashcard]
+    
+        //Create tap recognition for defLabel
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ExpandedFlashcard.tapFunction))
+        defLabel.frame = defLabel.bounds
+        defLabel.isUserInteractionEnabled = true
+        defLabel.addGestureRecognizer(tap)
+    
+        
+        //Hide label
+        defLabel.textColor = UIColor(named: "flipColorBack")
+        isHidden = true
+    
+        //Create tap recognition for defLabel
+        let tapTerm = UITapGestureRecognizer(target: self, action: #selector(ExpandedFlashcard.tapTermFunction))
+        termLabel.frame = defLabel.bounds
+        termLabel.isUserInteractionEnabled = true
+        termLabel.addGestureRecognizer(tapTerm)
+        
+        //Hide label
+        termLabel.textColor = UIColor(named: "flipColorBack")
+        isHidden = true
 
+    }
+    
+    @IBAction func tapFunction(sender: UITapGestureRecognizer) {
+        if isHidden == false{
+            defLabel.textColor = UIColor(named: "flipColorBack")
+            defLabel.backgroundColor = UIColor(named: "flipColorBack")
+            isHidden = true
+        }else{
+            defLabel.textColor = UIColor(named: "titleColor")
+            defLabel.backgroundColor = UIColor(named: "flipColorFront")
+            isHidden = false
+        }
+        
+    }
+    @IBAction func tapTermFunction(sender: UITapGestureRecognizer) {
+        if isHidden == false{
+            termLabel.textColor = UIColor(named: "flipColorBack")
+            termLabel.backgroundColor = UIColor(named: "flipColorBack")
+            isHidden = true
+        }else{
+            termLabel.textColor = UIColor(named: "titleColor")
+            termLabel.backgroundColor = UIColor(named: "flipColorFront")
+            isHidden = false
+        }
+        
+    }
 }
