6d6f14761b4b74e13737516d95313d0ca2958d97
Fara <farayan365@gmail.com>
2020-03-03 23:19:35 -0800

    Update colors

--- NoCrastinate/Onboarding.swift
+++ NoCrastinate/Onboarding.swift
@@ -63,8 +63,8 @@ class Onboarding: UIViewController{
 //Set content for onboarding screen
 extension Onboarding: PaperOnboardingDataSource {
     func onboardingItem(at index: Int) -> OnboardingItemInfo {
-        let bgOne = #colorLiteral(red: 0.7965814743, green: 0.9438976794, blue: 0.8768362138, alpha: 1)
-        let bgTwo = #colorLiteral(red: 0.8250316994, green: 1, blue: 0.9969279025, alpha: 1)
+        let bgOne = #colorLiteral(red: 0.9571279415, green: 0.9797350888, blue: 0.9580810356, alpha: 1)
+        let bgTwo = #colorLiteral(red: 0.9114294381, green: 0.9809204033, blue: 0.9893321701, alpha: 1)
         let bgThree = #colorLiteral(red: 0.8424793124, green: 0.9263140529, blue: 0.9921079278, alpha: 1)
         let bgFour = #colorLiteral(red: 0.8441429012, green: 0.8906594449, blue: 0.9921079278, alpha: 1)
         let bgFive = #colorLiteral(red: 0.8695143898, green: 0.8501115545, blue: 0.9921079278, alpha: 1)
